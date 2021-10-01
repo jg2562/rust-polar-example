9e7efe99d0a7774376d514b1d5d805ae512c691d
Jack Garrard <jg2562@nau.edu>
2021-09-30 22:45:29 -0700

    Added a super advanced calculation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod to_rust;
 use polars::{prelude::*, df};
 use pyo3::prelude::*;
 use crate::to_py::to_py_arrow;
+use crate::to_rust::to_rust_df;
 
 
 #[pyfunction]
@@ -15,8 +16,22 @@ fn create_df() -> PyResult<Vec<PyObject>> {
 	to_py_arrow(df)
 }
 
+fn calculation(series: &Series) -> Series {
+	series + 1
+}
+
+#[pyfunction]
+fn super_advanced_calculation(py_batches: Vec<&PyAny>) -> PyResult<Vec<PyObject>> {
+	let mut df = to_rust_df(py_batches)?;
+	df.apply("B", calculation).unwrap();
+	to_py_arrow(df)
+}
+
+
+
 #[pymodule]
 fn tapki_zero(_py: Python, m: &PyModule) -> PyResult<()> {
 	m.add_function(wrap_pyfunction!(create_df, m)?)?;
+	m.add_function(wrap_pyfunction!(super_advanced_calculation, m)?)?;
 	Ok(())
 }
